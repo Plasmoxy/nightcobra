0088d5ec62307191b35b0e909a5eadd4555e35b5
Plasmoxy <sebohacker@gmail.com>
2018-03-20 23:41:23 +0100

    asdasd

--- junk/hello.py
+++ junk/hello.py
@@ -2,12 +2,14 @@
 def decor(f):
     def wrap(x):
         print('===')
-        print('' = ' + f(x))
+        print('' + x + ' + f(x))')
         print('===')
     return wrap
+
 
 @decor
 def p(x):
     return x**2
+
 
 p(2)
