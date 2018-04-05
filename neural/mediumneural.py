43183f7898d371289974c3e6678c4f26cc5a9bd0
Plasmoxy <sebohacker@gmail.com>
2018-04-05 21:48:13 +0200

    sssss

--- neural/mediumneural.py
+++ neural/mediumneural.py
@@ -59,8 +59,18 @@ if __name__ == "__main__":
 
     # The training set. We have 4 examples, each consisting of 3 input values
     # and 1 output value.
-    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
-    training_set_outputs = array([[0, 1, 1, 0]]).T
+    training_set_inputs = array([
+    [0, 0, 1],
+    [1, 1, 1],
+    [1, 0, 1],
+    [0, 1, 1]
+    ])
+    training_set_outputs = array([[
+        0,
+        1,
+        1,
+        0
+    ]]).T
 
     # Train the neural network using a training set.
     # Do it 10,000 times and make small adjustments each time.
@@ -71,4 +81,4 @@ if __name__ == "__main__":
 
     # Test the neural network with a new situation.
     print("Considering new situation [1, 0, 0] -> ?: ")
-    print(neural_network.think(array([1, 0, 0])))
+    print(neural_network.think(array([0, 1, 1])))
