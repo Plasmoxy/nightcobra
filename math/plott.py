a294ed9cd761a7d72a95c80ab94becb205ad10ac
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-05-14 11:55:48 +0200

    xdd

---
+++ math/plott.py
@@ -0,0 +1,12 @@
+import pylab
+import numpy as np
+
+xspace = np.linspace(-10, 10, 100)
+
+sigmoid = lambda x: 100/(1 + np.e**-x)
+f = lambda x: x**2
+
+pylab.plot(xspace, sigmoid(xspace), )
+pylab.plot(xspace, f(xspace))
+
+pylab.show()
