39ae22680ba2a4348e392b481a3b9092dbee6a4d
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-05-14 08:03:25 +0200

    delet idea

---
+++ junk/hello.py
@@ -0,0 +1,15 @@
+
+def decor(f):
+    def wrap(x):
+        print('===')
+        print('' + x + ' + f(x))')
+        print('===')
+    return wrap
+
+
+@decor
+def p(x):
+    return x**2
+
+
+p(2)
---
+++ school/henlo.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+c = tk.Canvas(root)
+c.pack()
+
+class Player:
+    def __init__(self):
+        self.x = 50
+        self.y = 50
+        self.vx = 0
+        self.vy = 0
+        self.speed = 5
+
+    def draw(self):
+        c.create_oval(self.x-15, self.y-15, self.x+15, self.y+15, fill="black")
+
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+
+plr = Player()
+
+def loop():
+    global x, y
+    c.delete('all')
+
+    plr.move()
+    plr.draw()
+
+    c.update_idletasks()
+    c.update()
+    c.after(16, loop)
+
+def keydown(e):
+    code = e.keycode
+    if code == 37:
+        plr.vx = -plr.speed
+    elif code == 38 :
+        plr.vy = plr.speed
+    elif code == 39:
+        plr.vx = plr.speed
+
+root.bind('<KeyPress>', keydown)
+
+c.after(0, loop)
+root.focus_set()
+root.mainloop()
