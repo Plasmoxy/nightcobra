f47dec1564fb028f2a7fa5c22c8ae42a870f8ee3
Plasmoxy <sebohacker@gmail.com>
2018-05-19 00:12:39 +0200

    hmm cool

--- math/plott.py
+++ math/plott.py
@@ -3,13 +3,15 @@ import numpy as np
 
 xspace = np.linspace(-10, 10, 100)
 
-sigmoid = lambda x: 100/(1 + np.e**-x)
+sigmoid = lambda x: 1/(1 + np.e**-x)
 f = lambda x: x**2
 
 pylab.plot(xspace, sigmoid(xspace), )
 pylab.plot(xspace, f(xspace))
-pylab.plot(xspace, 100*pylab.sin(xspace))
+pylab.plot(xspace, pylab.sin(xspace))
 pylab.plot(3, sigmoid(3), 'ro')
 pylab.plot(3, sigmoid(3), 'ro')
 
+pylab.grid(True)
+pylab.ylim(-5, 20)
 pylab.show()
