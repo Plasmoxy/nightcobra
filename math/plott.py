131bf552f3f6706b2373bc284b6b5ad2f47fce02
Plasmoxy <sebohacker@gmail.com>
2018-05-19 17:50:27 +0200

    change lspace

--- math/plott.py
+++ math/plott.py
@@ -1,7 +1,7 @@
 import pylab
 import numpy as np
 
-xspace = np.linspace(-10, 10, 100)
+xspace = np.linspace(-5, 5, 100)
 
 sigmoid = lambda x: 1/(1 + np.e**-x)
 f = lambda x: x**2
@@ -9,9 +9,11 @@ f = lambda x: x**2
 pylab.plot(xspace, sigmoid(xspace), )
 pylab.plot(xspace, f(xspace))
 pylab.plot(xspace, pylab.sin(xspace))
+
+
 pylab.plot(3, sigmoid(3), 'ro')
-pylab.plot(3, sigmoid(3), 'ro')
+pylab.text(3, sigmoid(3) + 0.2, "A")
 
 pylab.grid(True)
-pylab.ylim(-5, 20)
+pylab.ylim(-2, 5)
 pylab.show()
