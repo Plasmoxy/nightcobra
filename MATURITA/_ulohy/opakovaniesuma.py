29f1e9a27aecc428bcbbdf4f7918e424b3be1680
Plasmoxy <sebohacker@gmail.com>
2019-09-12 13:47:49 +0200

    yyeet

--- MATURITA/_ulohy/opakovaniesuma.py
+++ MATURITA/_ulohy/opakovaniesuma.py
@@ -1,1 +1,15 @@
-asd
+# cvicenie na konverziu z eur do czk
+
+def main():
+    try:
+        eur = float(input("Zadajte sumu v EUR: "))
+    except:
+        print("Zadajte prosím číslo!")
+        return
+    
+    czk = 25 * eur
+
+    print(f"{eur} EUR je {czk} CZK")
+
+if __name__ == "__main__":
+    main()
